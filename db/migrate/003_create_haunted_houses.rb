d39474f2af60cf264f25127abc3641a1136d011d
Joseph Parker <jparker010190@gmail.com>
2018-05-05 20:50:14 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.boolean :family_friendly
       t.date :opening_date
       t.date :closing_date
+      t.string :description
       t.timestamps
     end
   end
