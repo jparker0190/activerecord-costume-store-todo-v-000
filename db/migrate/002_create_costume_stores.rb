324bc43fdf2fb7fb93277d3c00c641528d8513d1
Joseph Parker <jparker010190@gmail.com>
2018-05-05 20:38:02 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_store do |t|
+      t.string :name
+      t.integer :location
+      t.integer :customer_inventory
+      t.integer :employee_count
+      t.timestamps
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_house do |t|
+      t.string :name
+      t.integer :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.timestamps
+    end
+  end
+end
